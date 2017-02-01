b18e3e549b4e6d12242f894b626121eed59ee430
nicsuv141 <nicsuv141@studentmail.oru.se>
2017-02-01 11:41:36 +0100

    ds
    sdf

---
+++ ScrumprojektGrupp2/ScrumprojektGrupp2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScrumprojektGrupp2.MvcApplication" Language="C#" %>
---
+++ ScrumprojektGrupp2/ScrumprojektGrupp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScrumprojektGrupp2.Startup))]
+namespace ScrumprojektGrupp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
